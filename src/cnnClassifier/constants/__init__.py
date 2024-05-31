2b093d6acffe238d8b2bf96e6ed660bad0462719
Yash <yashkesharwani.india@gmail.com>
2024-06-01 02:02:32 +0530

    Data Ingestion

---
+++ src/cnnClassifier/constants/__init__.py
@@ -0,0 +1,4 @@
+from pathlib import Path
+
+CONFIG_FILE_PATH = Path('C:/Users/Yash/ml_project2/Kidney_Disease_Classification/config/config.yaml')
+PARAMS_FILE_PATH = Path('params.yaml')
--- template.py
+++ template.py
@@ -6,7 +6,7 @@ import logging
 logging.basicConfig(level = logging.INFO, format = '[%(asctime)s]: %(message)s')
 
 
-project_name = 'cnn-Classifier'
+project_name = 'cnnClassifier'
 
 
 list_of_files = [
