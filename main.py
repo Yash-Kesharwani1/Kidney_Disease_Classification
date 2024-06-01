286f0463ea30a4c33cd347c649755ffcf53aebcd
Yash <yashkesharwani.india@gmail.com>
2024-06-01 20:05:43 +0530

    Model Training Completed

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from cnnClassifier import logger
 from cnnClassifier.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
 from cnnClassifier.pipeline.stage_02_prepare_base_model import PrepareBaseModelTrainingPipeline
 from cnnClassifier.exception import CustomException
-
+from cnnClassifier.pipeline.stage_03_training_model import ModelTrainingPipeline
 
 STAGE_NAME = 'Data Ingestion stage'
 
@@ -25,5 +25,15 @@ try:
     obj.main()
     logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<<\n\nx===========================x")
 except Exception as e:
+    logger.exception(e)
+    raise e
+
+STAGE_NAME = 'Training'
+try:
+    logger.info(f"*********************************")
+    logger.info(f'>>>>>>> stage {STAGE_NAME} started <<<<<<<<<')
+    obj = ModelTrainingPipeline()
+    logger.info(f">>>>>>>>> stage {STAGE_NAME} completed <<<<<<<<<\n\nx=========================x")
+except CustomException as e:
     logger.exception(e)
     raise e
